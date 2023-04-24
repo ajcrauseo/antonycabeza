7e103c2c0611661cfa368999817d7e5a5b1f5d2c
Antony Cabeza <antonycabeza@gmail.com>
2023-04-24 18:30:02 -0400

    [Create] Works Section

--- public/assets/index.js
+++ public/assets/index.js
@@ -3,6 +3,7 @@ import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
+import web2 from "./web2.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
@@ -59,4 +60,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  web2
 };
