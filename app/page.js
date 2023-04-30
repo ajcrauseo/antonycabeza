7b5dbfddf41488d7c0a4cc3291223a4136d7ce9e
Antony Cabeza <antonycabeza@gmail.com>
2023-04-30 17:57:28 -0400

    [Update] Scrollbar styled

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ const { EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, EMAIL_JS_PUBLIC_KEY } =
 
 export default function HomePage() {
   return (
-    <main className='relative z-0 bg-primary'>
+    <main className='relative z-0 bg-primary '>
       <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
         <Navbar />
         <Hero />
